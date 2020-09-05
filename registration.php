d7edac14af3a6e72697db5ca18dbe33441b99f7e
Fabian Schmengler <fs@integer-net.de>
2020-09-05 13:53:33 +0200

    Replace placeholders

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    ':module-namespace_:module-name',
+    'IntegerNet_RewriteMap',
     __DIR__ . '/src'
 );
--- tests/unit/ExampleTest.php
+++ tests/unit/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace :module-namespace\:module-name;
+namespace IntegerNet\RewriteMap;
 
 use PHPUnit\Framework\TestCase;
 
